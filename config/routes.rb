b396bc5b3f3bba7404a7d3ae46a50818a0d52c7d
Vedant Lohbare <vedant.lohbare@gmail.com>
2019-12-28 23:01:38 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'static/about'
+  get 'static/contact'
+  resources :blogs
+
+  root to: 'static#home'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
